43ee5d9344b79a58b996568cdfe956ec746eab40
Erik Perri <erik.perri@gmail.com>
2026-03-06 08:21:46 -0500

    update: add restructure after parse

--- src/main.rs
+++ src/main.rs
@@ -17,12 +17,23 @@ fn main() {
         process::exit(1);
     });
 
-    println!("{:#?}", packets);
+    let set = combine_set(packets).unwrap_or_else(|e| {
+        println!("Failed to combine set: {}", e);
+        process::exit(1);
+    });
+
+    println!("{:#?}", set);
 }
 
 enum Par2Error {
     Io(std::io::Error),
     ParseError(String),
+    DuplicateMainPacket,
+    MissingComputedMD5,
+    MissingFileDescriptions,
+    MissingMainPacket,
+    MissingRecoverySetId,
+    MissingSliceChecksums,
 }
 
 impl From<std::io::Error> for Par2Error {
@@ -42,6 +53,27 @@ impl std::fmt::Display for Par2Error {
         match self {
             Par2Error::Io(err) => write!(f, "IO error: {}", err),
             Par2Error::ParseError(message) => write!(f, "Parse error: {}", message),
+            Par2Error::DuplicateMainPacket => write!(f, "Duplicate main packet"),
+            Par2Error::MissingComputedMD5 => write!(f, "Missing computed MD5"),
+            Par2Error::MissingFileDescriptions => write!(f, "Missing file descriptions"),
+            Par2Error::MissingMainPacket => write!(f, "Missing main packet"),
+            Par2Error::MissingRecoverySetId => write!(f, "Missing recovery set ID"),
+            Par2Error::MissingSliceChecksums => write!(f, "Missing slice checksums"),
+        }
+    }
+}
+
+#[derive(Debug)]
+enum Par2Warning {
+    MissingCreator,
+    UnknownPacketType,
+}
+
+impl std::fmt::Display for Par2Warning {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Par2Warning::MissingCreator => write!(f, "Missing creator"),
+            Par2Warning::UnknownPacketType => write!(f, "Unknown packet type"),
         }
     }
 }
@@ -108,6 +140,23 @@ struct Par2RecoverySliceData {
 #[derive(Debug)]
 struct Par2CreatorData {
     name: Vec<u8>,
+}
+
+#[derive(Debug)]
+struct Parsed<T> {
+    expected_md5: Par2Md5Hash,
+    computed_md5: Par2Md5Hash,
+    data: T,
+}
+
+#[derive(Debug)]
+struct Par2Set {
+    recovery_set_id: [u8; 16],
+    main: Parsed<Par2MainData>,
+    file_descriptions: Vec<Parsed<Par2FileDescriptionData>>,
+    slice_checksums: Vec<Parsed<Par2SliceChecksumData>>,
+    recovery_slices: Vec<Parsed<Par2RecoverySliceData>>,
+    creators: Vec<Parsed<Par2CreatorData>>,
 }
 
 const PAR2_PACKET_MAGIC_HEADER: &[u8] = b"PAR2\0PKT";
@@ -402,4 +451,91 @@ fn parse_creator(data: &[u8]) -> Result<Par2PacketBody, Par2Error> {
     file_name.truncate(trailing_null_bytes);
 
     Ok(Par2PacketBody::Creator(Par2CreatorData { name: file_name }))
+}
+
+fn combine_set(packets: Vec<Par2Packet>) -> Result<(Par2Set, Vec<Par2Warning>), Par2Error> {
+    let mut recovery_set_id = None;
+    let mut main: Option<Parsed<Par2MainData>> = None;
+    let mut file_descriptions = Vec::new();
+    let mut slice_checksums = Vec::new();
+    let mut recovery_slices = Vec::new();
+    let mut creators = Vec::new();
+    let mut warnings = Vec::new();
+
+    for packet in packets {
+        let computed_md5 = packet
+            .header
+            .computed_md5
+            .ok_or(Par2Error::MissingComputedMD5)?;
+
+        match packet.body {
+            Par2PacketBody::Main(data) => {
+                if let Some(main) = main.as_ref() {
+                    if main.expected_md5 != packet.header.expected_md5
+                        || main.computed_md5 != computed_md5
+                        || packet.header.recovery_set_id != recovery_set_id.unwrap()
+                    {
+                        return Err(Par2Error::DuplicateMainPacket);
+                    }
+
+                    continue;
+                }
+
+                recovery_set_id = Some(packet.header.recovery_set_id);
+
+                main = Some(Parsed {
+                    expected_md5: packet.header.expected_md5,
+                    computed_md5,
+                    data,
+                });
+            }
+            Par2PacketBody::FileDesc(data) => file_descriptions.push(Parsed {
+                expected_md5: packet.header.expected_md5,
+                computed_md5,
+                data,
+            }),
+            Par2PacketBody::SliceChecksum(data) => slice_checksums.push(Parsed {
+                expected_md5: packet.header.expected_md5,
+                computed_md5,
+                data,
+            }),
+            Par2PacketBody::RecoverySlice(data) => recovery_slices.push(Parsed {
+                expected_md5: packet.header.expected_md5,
+                computed_md5,
+                data,
+            }),
+            Par2PacketBody::Creator(data) => creators.push(Parsed {
+                expected_md5: packet.header.expected_md5,
+                computed_md5,
+                data,
+            }),
+            Par2PacketBody::Unknown(_) => {
+                warnings.push(Par2Warning::UnknownPacketType);
+            }
+        }
+    }
+
+    if creators.is_empty() {
+        warnings.push(Par2Warning::MissingCreator);
+    }
+
+    if file_descriptions.is_empty() {
+        return Err(Par2Error::MissingFileDescriptions);
+    }
+
+    if slice_checksums.is_empty() {
+        return Err(Par2Error::MissingSliceChecksums);
+    }
+
+    Ok((
+        Par2Set {
+            recovery_set_id: recovery_set_id.ok_or(Par2Error::MissingRecoverySetId)?,
+            main: main.ok_or(Par2Error::MissingMainPacket)?,
+            file_descriptions,
+            slice_checksums,
+            recovery_slices,
+            creators,
+        },
+        warnings,
+    ))
 }
